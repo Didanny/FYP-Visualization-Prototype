d7979208ee85f5304bd8af4a20d145de9e0e195f
Danny Abraham (Student) <daa50@aub.edu.lb>
2019-02-28 13:10:04 +0200

    Add generic button handler for menus

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,26 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonHandler : MonoBehaviour, IInputClickHandler
+{
+    [SerializeField] UnityEvent response;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        response.Invoke();
+        Destroy(gameObject.transform.parent);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
