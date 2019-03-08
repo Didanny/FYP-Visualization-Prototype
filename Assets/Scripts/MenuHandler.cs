45384b3a004b4d085705bf462c9f67eb4d3b1836
Danny Abraham (Student) <daa50@aub.edu.lb>
2019-03-08 12:29:00 +0200

    Start settings menu

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void HideMenu()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ShowMenu()
+    {
+        gameObject.SetActive(true);
+    }
+}
