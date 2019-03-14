99207a3f4a490c43be32fab3b89ba0ab299a1096
Didanny <37907412+Didanny@users.noreply.github.com>
2019-03-14 19:56:14 +0200

    Fix speech input bug

--- Assets/Scripts/MenusManager.cs
+++ Assets/Scripts/MenusManager.cs
@@ -8,26 +8,28 @@ public class MenusManager : MonoBehaviour {
     [SerializeField] GameObject MainMenu;
     [SerializeField] GameObject SettingsMenu;
 
-	// Use this for initialization
-	void Start () {
-        Invoke("HideMain", 5);
-        Invoke("ShowMain", 10);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        // Invoke("HideMain", 5);
+        // Invoke("ShowMain", 10);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
 
     // Show MainMenu
     public void ShowMain()
     {
+        Debug.Log("MenusManager: Show Main Menu");
         MainMenu.SetActive(true);
     }
 
     // Hide MainMenu
     public void HideMain()
     {
+        Debug.Log("MenusManager: Hide Main Menu");
         MainMenu.SetActive(false);
     }
 
