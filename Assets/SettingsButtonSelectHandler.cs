7df258eabf1f8d4bf47b3f1c5919b3f5018a6e8a
Danny Abraham (Student) <daa50@aub.edu.lb>
2019-03-16 20:44:05 +0200

    Fix settings button issue with hiding main

--- Assets/SettingsButtonSelectHandler.cs
+++ Assets/SettingsButtonSelectHandler.cs
@@ -12,7 +12,6 @@ public class SettingsButtonSelectHandler : MonoBehaviour, IInputClickHandler
 
     public void OnInputClicked(InputClickedEventData eventData)
     {
-        gameObject.SetActive(false);
         ShowSettings.Invoke();
     }
 
