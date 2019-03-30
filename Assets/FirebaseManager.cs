6fc9079cd29abffcc82c95c9121315f538138bde
Danny Abraham (Student) <daa50@aub.edu.lb>
2019-03-30 17:50:01 +0200

    Import firebase assets

---
+++ Assets/FirebaseManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleFirebaseUnity;
+
+public class FirebaseManager : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+        
+        var firebase = Firebase.CreateNew("https://fyp-warehouse.firebaseio.com/");
+
+        firebase.Child("Messages").Push("{ \"name\": \"simple-firebase-unity\", \"message\": \"awesome!\"}", true);
+
+        firebase.OnDeleteSuccess += (Firebase sender, DataSnapshot snapshot) => {
+            Debug.Log("[OK] Delete from " + sender.Endpoint + ": " + snapshot.RawJson);
+        };
+
+        //firebase.OnUpdateFailed += UpdateFailedHandler;
+        // Method signature: void UpdateFailedHandler(Firebase sender, FirebaseError err)
+
+        firebase.GetValue("print=pretty");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
